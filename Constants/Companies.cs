70408dbd8b50192840fac713e416ca325e9d8483
Panayot Ivanov <panayot.zhi@gmail.com>
2021-02-13 12:28:16 +0200

    Separate into different constant files.

--- Constants/Companies.cs
+++ Constants/Companies.cs
@@ -2,6 +2,64 @@
 {
     public static class Companies
     {
-       
+        /*public const string Brooklyn14th = "5th Connecticut";
+ public const string Brooklyn14th = "16th Connecticut";
+
+ public const string Brooklyn14th = "1st Delaware Company A";
+ public const string Brooklyn14th = "2nd Delaware Company A";
+
+ public const string Brooklyn14th = "8th Illinois Company A";
+ public const string Brooklyn14th = "65th Illinois Company B";
+ public const string Brooklyn14th = "2nd Delaware Company A";
+ public const string Brooklyn14th = "2nd Delaware Company A";*/
+
+        /*public const string Brooklyn14th = "14th Brooklyn";
+        public const string Delaware1st = "1st Delaware";
+        public const string Illinois65th = "65th Illinois";
+        public const string Indiana14th = "14th Indiana";
+        public const string Indiana19th = "19th Indiana";
+        public const string Maine20th = "20th Maine";
+        public const string Maine7th = "7th Maine";
+        public const string Maryland1st = "1st Maryland";
+        public const string Maryland3rd = "3rd Maryland";
+        public const string Maryland2nd = "2nd Maryland";
+        public const string Massachusetts15th = "15th Massachusetts";
+        public const string Massachusetts28th = "28th Massachusetts";
+        public const string Michigan17th = "17th Michigan";
+        public const string Michigan7th = "7th Michigan";
+        public const string Minnesota1st = "1st Minnesota";
+        public const string NewYork126th = "126th New York";
+        public const string NewYork20th = "20th New York";
+        public const string NewYork23rd = "23rd New York";
+        public const string NewYork39th = "39th New York";
+        public const string NewYork51st = "51st New York";
+        public const string NewYork52nd = "52nd New York";
+        public const string NewYork5th = "5th New York";
+        public const string NewYork69th = "69th New York";
+        public const string NewYork89th = "89th New York";
+        public const string NewYork9th = "9th New York";
+        public const string NewYorkMilitia12th = "12th New York Militia";
+        public const string Ohio12th = "12th Ohio";
+        public const string Ohio23rd = "23rd Ohio";
+        public const string Ohio30th = "30th Ohio";
+        public const string Ohio32nd = "32nd Ohio";
+        public const string Ohio36th = "36th Ohio";
+        public const string Ohio87th = "87th Ohio";
+        public const string Ohio8th = "8th Ohio";
+        public const string Pennsylvania114th = "114th Pennsylvania";
+        public const string Pennsylvania28th = "28th Pennsylvania";
+        public const string Pennsylvania32nd = "32nd Pennsylvania";
+        public const string Pennsylvania42nd = "42nd Pennsylvania";
+        public const string Pennsylvania4th = "4th Pennsylvania";
+        public const string Pennsylvania51st = "51st Pennsylvania";
+        public const string Pennsylvania6th = "6th Pennsylvania";
+        public const string Pennsylvania72nd = "72nd Pennsylvania";
+        public const string USSharpshooters1st = "1st US Sharpshooters";
+        public const string USSharpshooters2nd = "2nd US Sharpshooters";
+        public const string UnitedStates10th = "10th United States";
+        public const string UnitedStates2nd = "2nd United States";
+        public const string Vermont9th = "9th Vermont";
+        public const string Wisconsin2nd = "2nd Wisconsin";
+        public const string Wisconsin6th = "6th Wisconsin";*/
     }
 }
--- Constants/Weapons.cs
+++ Constants/Weapons.cs
@@ -2,6 +2,13 @@
 {
     public static class Weapons
     {
-        
+        public const string SpringfieldM18642 = "Springfield M1842";
+        public const string SpringfieldM18655 = "Springfield M1855";
+        public const string SpringfieldM18661 = "Springfield M1861";
+        public const string PatternEnfieldM1853 = "Pattern Enfield M1853";
+        public const string LorenzM1854 = "Lorenz M1854";
+        public const string MississippiM1841 = "Mississippi M1841";
+        public const string SharpsRifleM1859 = "Sharps Rifle M1859";
+        public const string WhitworthRifle = "Whitworth Rifle";
     }
 }
