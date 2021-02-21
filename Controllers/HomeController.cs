d14f5f7f0013a23de4209e3895c46a2e9ea771fa
Panayot Ivanov <panayot.zhi@gmail.com>
2021-02-21 19:41:52 +0200

    Split these fiels.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Dynamic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using WarOfRightsWeb.Models;
 
 namespace WarOfRightsWeb.Controllers
@@ -8,18 +12,17 @@ namespace WarOfRightsWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnv;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnv, IConfiguration configuration)
         {
             _logger = logger;
+            _webHostEnv = webHostEnv;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
@@ -28,6 +31,19 @@ namespace WarOfRightsWeb.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult About()
+        {
+            dynamic vModel = new ExpandoObject();
+
+            var filePath = Path.Combine(_webHostEnv.WebRootPath, "html", $"{_configuration["CompanyID"]}.html");
+            var content = System.IO.File.ReadAllText(filePath);
+            var roosterFile = new FileInfo(filePath);
+            vModel.Retrieved = roosterFile.LastWriteTime.ToString("f");
+            vModel.Content = content;
+
+            return View(vModel);
         }
     }
 }
---
+++ Controllers/RegimentsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Dynamic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WarOfRightsWeb.Controllers
+{
+    public class RegimentsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _webHostEnv;
+
+        public RegimentsController(IConfiguration configuration, IWebHostEnvironment webHostEnv)
+        {
+            this._configuration = configuration;
+            this._webHostEnv = webHostEnv;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult USA()
+        {
+            return View();
+        }
+
+        public IActionResult CSA()
+        {
+            return View();
+        }
+    }
+}
