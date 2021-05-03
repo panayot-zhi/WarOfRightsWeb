157bdbaedcbf71353d6878fdec793ed6facf3cd7
Panayot Ivanov <panayot_zhi@abv.bg>
2021-05-03 19:07:22 +0300

    Add local date func and timer.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,15 +3,18 @@ function isValidDate(d) {
     return d instanceof Date && !isNaN(d);
 }
 
-function calculateDates() {
-    var dateFields = $(".local-date").text();
+function format(time) {
+    // Hours, minutes and seconds
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = ~~time % 60;
 
-    for (var dateField in dateFields) {
-        var date = new Date(dateField);
-        if (!isValidDate(date)) {
-            continue;
-        }
-
-        // TODO
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
     }
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
 }
