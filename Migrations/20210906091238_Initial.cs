09bfda76c0c9b25980ddf7f7aef483f7739d4340
Panayot Ivanov <panayot_zhi@abv.bg>
2021-09-06 12:19:04 +0300

    Clean migrations, mark migration as initial.

---
+++ Migrations/20210906091238_Initial.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfRightsWeb.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "maps",
+                columns: table => new
+                {
+                    id = table.Column<string>(maxLength: 255, nullable: false),
+                    name = table.Column<string>(maxLength: 255, nullable: false),
+                    area_name = table.Column<string>(maxLength: 255, nullable: true),
+                    description = table.Column<string>(type: "TEXT", nullable: true),
+                    date_time_description = table.Column<string>(maxLength: 255, nullable: true),
+                    defending_team = table.Column<string>(maxLength: 127, nullable: true),
+                    transfer_on_death = table.Column<decimal>(type: "DECIMAL(6,4)", nullable: true),
+                    round_time = table.Column<int>(nullable: true),
+                    wave_time = table.Column<int>(nullable: true),
+                    capture_speed = table.Column<decimal>(type: "DECIMAL(6,4)", nullable: true),
+                    neutralize_speed = table.Column<decimal>(type: "DECIMAL(6,4)", nullable: true),
+                    tickets_usa = table.Column<int>(nullable: true),
+                    tickets_csa = table.Column<int>(nullable: true),
+                    final_push_time = table.Column<int>(nullable: true),
+                    skirmish_image_path = table.Column<string>(maxLength: 255, nullable: true),
+                    spawn_image_path = table.Column<string>(maxLength: 255, nullable: true),
+                    loading_image_path = table.Column<string>(maxLength: 255, nullable: true),
+                    order = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_maps", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "regiments",
+                columns: table => new
+                {
+                    id = table.Column<string>(maxLength: 255, nullable: false),
+                    name = table.Column<string>(maxLength: 255, nullable: true),
+                    faction = table.Column<string>(maxLength: 127, nullable: true),
+                    state = table.Column<string>(maxLength: 127, nullable: true),
+                    number = table.Column<string>(maxLength: 127, nullable: true),
+                    description = table.Column<string>(type: "TEXT", nullable: true),
+                    type = table.Column<string>(maxLength: 127, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_regiments", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "weapons",
+                columns: table => new
+                {
+                    id = table.Column<string>(maxLength: 255, nullable: false),
+                    name = table.Column<string>(maxLength: 255, nullable: false),
+                    description = table.Column<string>(type: "TEXT", nullable: true),
+                    parameters_description = table.Column<string>(maxLength: 512, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_weapons", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "map_regiment",
+                columns: table => new
+                {
+                    id = table.Column<string>(nullable: false),
+                    map_id = table.Column<string>(nullable: true),
+                    regiment_id = table.Column<string>(nullable: true),
+                    order = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_map_regiment", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_map_regiment_maps_map_id",
+                        column: x => x.map_id,
+                        principalTable: "maps",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_map_regiment_regiments_regiment_id",
+                        column: x => x.regiment_id,
+                        principalTable: "regiments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "map_regiment_weapon",
+                columns: table => new
+                {
+                    id = table.Column<string>(nullable: false),
+                    map_regiment_id = table.Column<string>(nullable: true),
+                    weapon_id = table.Column<string>(nullable: true),
+                    count = table.Column<int>(nullable: true),
+                    percent = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_map_regiment_weapon", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_map_regiment_weapon_map_regiment_map_regiment_id",
+                        column: x => x.map_regiment_id,
+                        principalTable: "map_regiment",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "fk_map_regiment_weapon_weapons_weapon_id",
+                        column: x => x.weapon_id,
+                        principalTable: "weapons",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_map_regiment_regiment_id",
+                table: "map_regiment",
+                column: "regiment_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_map_regiment_map_id_regiment_id",
+                table: "map_regiment",
+                columns: new[] { "map_id", "regiment_id" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_map_regiment_weapon_weapon_id",
+                table: "map_regiment_weapon",
+                column: "weapon_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_map_regiment_weapon_map_regiment_id_weapon_id",
+                table: "map_regiment_weapon",
+                columns: new[] { "map_regiment_id", "weapon_id" },
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "map_regiment_weapon");
+
+            migrationBuilder.DropTable(
+                name: "map_regiment");
+
+            migrationBuilder.DropTable(
+                name: "weapons");
+
+            migrationBuilder.DropTable(
+                name: "maps");
+
+            migrationBuilder.DropTable(
+                name: "regiments");
+        }
+    }
+}
--- Migrations/WarOfRightsDbContextModelSnapshot.cs
+++ Migrations/WarOfRightsDbContextModelSnapshot.cs
@@ -2,17 +2,15 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WarOfRightsWeb.Data;
 
-namespace WarOfRightsWeb.Data.Migrations
+namespace WarOfRightsWeb.Migrations
 {
     [DbContext(typeof(WarOfRightsDbContext))]
-    [Migration("20210411103359_Initial")]
-    partial class Initial
+    partial class WarOfRightsDbContextModelSnapshot : ModelSnapshot
     {
-        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -22,190 +20,253 @@ namespace WarOfRightsWeb.Data.Migrations
             modelBuilder.Entity("WarOfRightsWeb.Data.Map", b =>
                 {
                     b.Property<string>("ID")
+                        .HasColumnName("id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<string>("AreaName")
+                        .HasColumnName("area_name")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<decimal?>("CaptureSpeed")
+                        .HasColumnName("capture_speed")
                         .HasColumnType("DECIMAL(6,4)");
 
                     b.Property<string>("DateTimeDescription")
+                        .HasColumnName("date_time_description")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<string>("DefendingTeam")
-                        .HasColumnType("varchar(128) CHARACTER SET utf8mb4")
-                        .HasMaxLength(128);
+                        .HasColumnName("defending_team")
+                        .HasColumnType("varchar(127) CHARACTER SET utf8mb4")
+                        .HasMaxLength(127);
 
                     b.Property<string>("Description")
+                        .HasColumnName("description")
                         .HasColumnType("TEXT");
 
                     b.Property<int?>("FinalPushTime")
+                        .HasColumnName("final_push_time")
                         .HasColumnType("int");
 
                     b.Property<string>("LoadingImagePath")
+                        .HasColumnName("loading_image_path")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<string>("Name")
                         .IsRequired()
+                        .HasColumnName("name")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<decimal?>("NeutralizeSpeed")
+                        .HasColumnName("neutralize_speed")
                         .HasColumnType("DECIMAL(6,4)");
 
+                    b.Property<int>("Order")
+                        .HasColumnName("order")
+                        .HasColumnType("int");
+
                     b.Property<int?>("RoundTime")
+                        .HasColumnName("round_time")
                         .HasColumnType("int");
 
                     b.Property<string>("SkirmishImagePath")
+                        .HasColumnName("skirmish_image_path")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<string>("SpawnImagePath")
+                        .HasColumnName("spawn_image_path")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<int?>("TicketsCSA")
+                        .HasColumnName("tickets_csa")
                         .HasColumnType("int");
 
                     b.Property<int?>("TicketsUSA")
+                        .HasColumnName("tickets_usa")
                         .HasColumnType("int");
 
                     b.Property<decimal?>("TransferOnDeath")
+                        .HasColumnName("transfer_on_death")
                         .HasColumnType("DECIMAL(6,4)");
 
                     b.Property<int?>("WaveTime")
+                        .HasColumnName("wave_time")
                         .HasColumnType("int");
 
-                    b.HasKey("ID");
+                    b.HasKey("ID")
+                        .HasName("pk_maps");
 
-                    b.ToTable("Maps");
+                    b.ToTable("maps");
                 });
 
             modelBuilder.Entity("WarOfRightsWeb.Data.MapRegiment", b =>
                 {
                     b.Property<string>("ID")
+                        .HasColumnName("id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4");
 
                     b.Property<string>("MapID")
+                        .HasColumnName("map_id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4");
+
+                    b.Property<int>("Order")
+                        .HasColumnName("order")
+                        .HasColumnType("int");
 
                     b.Property<string>("RegimentID")
+                        .HasColumnName("regiment_id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4");
 
-                    b.HasKey("ID");
+                    b.HasKey("ID")
+                        .HasName("pk_map_regiment");
 
-                    b.HasIndex("RegimentID");
+                    b.HasIndex("RegimentID")
+                        .HasName("ix_map_regiment_regiment_id");
 
                     b.HasIndex("MapID", "RegimentID")
-                        .IsUnique();
+                        .IsUnique()
+                        .HasName("ix_map_regiment_map_id_regiment_id");
 
-                    b.ToTable("MapRegiment");
+                    b.ToTable("map_regiment");
                 });
 
             modelBuilder.Entity("WarOfRightsWeb.Data.MapRegimentWeapon", b =>
                 {
                     b.Property<string>("ID")
+                        .HasColumnName("id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4");
 
                     b.Property<int?>("Count")
+                        .HasColumnName("count")
                         .HasColumnType("int");
 
                     b.Property<string>("MapRegimentID")
+                        .HasColumnName("map_regiment_id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4");
 
                     b.Property<int?>("Percent")
+                        .HasColumnName("percent")
                         .HasColumnType("int");
 
                     b.Property<string>("WeaponID")
+                        .HasColumnName("weapon_id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4");
 
-                    b.HasKey("ID");
+                    b.HasKey("ID")
+                        .HasName("pk_map_regiment_weapon");
 
-                    b.HasIndex("WeaponID");
+                    b.HasIndex("WeaponID")
+                        .HasName("ix_map_regiment_weapon_weapon_id");
 
                     b.HasIndex("MapRegimentID", "WeaponID")
-                        .IsUnique();
+                        .IsUnique()
+                        .HasName("ix_map_regiment_weapon_map_regiment_id_weapon_id");
 
-                    b.ToTable("MapRegimentWeapon");
+                    b.ToTable("map_regiment_weapon");
                 });
 
             modelBuilder.Entity("WarOfRightsWeb.Data.Regiment", b =>
                 {
                     b.Property<string>("ID")
+                        .HasColumnName("id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<string>("Description")
+                        .HasColumnName("description")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Faction")
-                        .HasColumnType("varchar(128) CHARACTER SET utf8mb4")
-                        .HasMaxLength(128);
+                        .HasColumnName("faction")
+                        .HasColumnType("varchar(127) CHARACTER SET utf8mb4")
+                        .HasMaxLength(127);
 
                     b.Property<string>("Name")
+                        .HasColumnName("name")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<string>("Number")
-                        .HasColumnType("longtext CHARACTER SET utf8mb4");
+                        .HasColumnName("number")
+                        .HasColumnType("varchar(127) CHARACTER SET utf8mb4")
+                        .HasMaxLength(127);
 
                     b.Property<string>("State")
-                        .HasColumnType("varchar(128) CHARACTER SET utf8mb4")
-                        .HasMaxLength(128);
+                        .HasColumnName("state")
+                        .HasColumnType("varchar(127) CHARACTER SET utf8mb4")
+                        .HasMaxLength(127);
 
-                    b.HasKey("ID");
+                    b.Property<string>("Type")
+                        .IsRequired()
+                        .HasColumnName("type")
+                        .HasColumnType("varchar(127) CHARACTER SET utf8mb4")
+                        .HasMaxLength(127);
 
-                    b.ToTable("Regiments");
+                    b.HasKey("ID")
+                        .HasName("pk_regiments");
+
+                    b.ToTable("regiments");
                 });
 
             modelBuilder.Entity("WarOfRightsWeb.Data.Weapon", b =>
                 {
                     b.Property<string>("ID")
+                        .HasColumnName("id")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<string>("Description")
+                        .HasColumnName("description")
                         .HasColumnType("TEXT");
 
                     b.Property<string>("Name")
                         .IsRequired()
+                        .HasColumnName("name")
                         .HasColumnType("varchar(255) CHARACTER SET utf8mb4")
                         .HasMaxLength(255);
 
                     b.Property<string>("ParametersDescription")
+                        .HasColumnName("parameters_description")
                         .HasColumnType("varchar(512) CHARACTER SET utf8mb4")
                         .HasMaxLength(512);
 
-                    b.HasKey("ID");
+                    b.HasKey("ID")
+                        .HasName("pk_weapons");
 
-                    b.ToTable("Weapons");
+                    b.ToTable("weapons");
                 });
 
             modelBuilder.Entity("WarOfRightsWeb.Data.MapRegiment", b =>
                 {
                     b.HasOne("WarOfRightsWeb.Data.Map", "Map")
                         .WithMany("MapRegiments")
-                        .HasForeignKey("MapID");
+                        .HasForeignKey("MapID")
+                        .HasConstraintName("fk_map_regiment_maps_map_id");
 
                     b.HasOne("WarOfRightsWeb.Data.Regiment", "Regiment")
                         .WithMany("MapRegiments")
-                        .HasForeignKey("RegimentID");
+                        .HasForeignKey("RegimentID")
+                        .HasConstraintName("fk_map_regiment_regiments_regiment_id");
                 });
 
             modelBuilder.Entity("WarOfRightsWeb.Data.MapRegimentWeapon", b =>
                 {
                     b.HasOne("WarOfRightsWeb.Data.MapRegiment", "MapRegiment")
                         .WithMany("MapRegimentWeapons")
-                        .HasForeignKey("MapRegimentID");
+                        .HasForeignKey("MapRegimentID")
+                        .HasConstraintName("fk_map_regiment_weapon_map_regiment_map_regiment_id");
 
                     b.HasOne("WarOfRightsWeb.Data.Weapon", "Weapon")
                         .WithMany("MapRegimentWeapons")
-                        .HasForeignKey("WeaponID");
+                        .HasForeignKey("WeaponID")
+                        .HasConstraintName("fk_map_regiment_weapon_weapons_weapon_id");
                 });
 #pragma warning restore 612, 618
         }
