0c2260a9b6edaaf1f1bd08927feda279be911c11
Panayot Ivanov <panayot_zhi@abv.bg>
2022-03-05 12:39:01 +0200

    Add migrations.

---
+++ Data/Migrations/20220209194530_CompaniesAndMapNarrator.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfRightsWeb.Migrations
+{
+    public partial class CompaniesAndMapNarrator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "companies",
+                table: "regiments",
+                maxLength: 127,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "narrator_info",
+                table: "maps",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "companies",
+                table: "regiments");
+
+            migrationBuilder.DropColumn(
+                name: "narrator_info",
+                table: "maps");
+        }
+    }
+}
---
+++ Data/Migrations/20220304193926_RemoveWeaponPercent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WarOfRightsWeb.Migrations
+{
+    public partial class RemoveWeaponPercent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "percent",
+                table: "map_regiment_weapon");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "percent",
+                table: "map_regiment_weapon",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
