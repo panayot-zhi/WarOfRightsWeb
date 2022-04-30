d5d13b9606d9f444a00ca1ae179f04a038e95de2
Panayot Ivanov <panayot_zhi@abv.bg>
2022-04-30 20:18:02 +0300

    Refactor, refine.

--- Controllers/MapsController.cs
+++ Controllers/MapsController.cs
@@ -24,7 +24,7 @@ namespace WarOfRightsWeb.Controllers
 
         public async Task<IActionResult> Antietam(string id, string mode)
         {
-            var area = Maps.Areas.Antietam;
+            const string area = Labels.Antietam;
 
             if (!string.IsNullOrEmpty(id))
             {
@@ -43,7 +43,7 @@ namespace WarOfRightsWeb.Controllers
 
         public async Task<IActionResult> HarpersFerry(string id, string mode)
         {
-            var area = Maps.Areas.HarpersFerry;
+            const string area = Labels.HarpersFerry;
 
             if (!string.IsNullOrEmpty(id))
             {
@@ -61,7 +61,7 @@ namespace WarOfRightsWeb.Controllers
 
         public async Task<IActionResult> SouthMountain(string id, string mode)
         {
-            var area = Maps.Areas.SouthMountain;
+            const string area = Labels.SouthMountain;
 
             if (!string.IsNullOrEmpty(id))
             {
@@ -79,7 +79,7 @@ namespace WarOfRightsWeb.Controllers
 
         public async Task<IActionResult> DrillCamps(string id, string mode)
         {
-            var area = Maps.Areas.DrillCamps;
+            const string area = Labels.DrillCamps;
 
             if (!string.IsNullOrEmpty(id))
             {
@@ -125,60 +125,63 @@ namespace WarOfRightsWeb.Controllers
             var maps = _db.Maps
                 .AsNoTracking();
 
+            if (Labels.DrillCamps.Equals(area))
+            {
+                if (mode == Labels.DrillCamp)
+                {
+                    return maps.Where(x => Maps.DrillCampMaps.Contains(x.ID))
+                        .OrderBy(x => x.Order)
+                        .Select(x => SelectMap(x));
+                }
+
+                if (mode == Labels.Conquest)
+                {
+                    return maps.Where(x => x.MapType == Labels.Conquest 
+                            && x.AreaName == Labels.DrillCamps)
+                        .OrderBy(x => x.Order)
+                        .Select(x => SelectMap(x));
+                }
+
+                // else --> return all drill camp maps
+                return maps.Where(x => Maps.DrillCampMaps.Contains(x.ID) || 
+                            x.MapType == Labels.Conquest && x.AreaName == Labels.DrillCamps)
+                    .OrderBy(x => x.Name)
+                    .Select(x => SelectMap(x));
+            }
+
             if (!string.IsNullOrEmpty(mode))
             {
                 maps = maps.Where(x => x.MapType == mode);
             }
-            
-            if (Maps.Areas.DrillCamps.Equals(area))
-            {
-                return maps.Where(x => Maps.DrillCampMaps.Contains(x.ID))
-                    .OrderBy(x => x.Order)
-                    .Select(x => new Map()
-                    {
-                        ID = x.ID,
-                        Name = x.Name,
-                        AreaName = x.AreaName,
-                        DateTimeDescription = x.DateTimeDescription,
-                        DefendingTeam = x.DefendingTeam,
-                        Description = x.Description,
-                        SkirmishImagePath = x.SkirmishImagePath,
-                    });
-            }
 
-            if (Maps.Areas.Antietam.Equals(area) ||
-                Maps.Areas.SouthMountain.Equals(area) ||
-                Maps.Areas.HarpersFerry.Equals(area))
+            if (Labels.Antietam.Equals(area) ||
+                Labels.SouthMountain.Equals(area) ||
+                Labels.HarpersFerry.Equals(area))
             {
                 return maps.Where(x => x.AreaName == area)
                     .OrderBy(x => x.Order)
-                    .Select(x => new Map()
-                    {
-                        ID = x.ID,
-                        Name = x.Name,
-                        AreaName = x.AreaName,
-                        DateTimeDescription = x.DateTimeDescription,
-                        DefendingTeam = x.DefendingTeam,
-                        Description = x.Description,
-                        SkirmishImagePath = x.SkirmishImagePath,
-                    });
+                    .Select(x => SelectMap(x));
             }
 
             // else return all 
             // maps ordered by name
             return maps
                 .OrderBy(x => x.Name)
-                .Select(x => new Map()
-                {
-                    ID = x.ID,
-                    Name = x.Name,
-                    AreaName = x.AreaName,
-                    DateTimeDescription = x.DateTimeDescription,
-                    DefendingTeam = x.DefendingTeam,
-                    Description = x.Description,
-                    SkirmishImagePath = x.SkirmishImagePath
-                });
+                .Select(x => SelectMap(x));
         }
 
+        private static Map SelectMap(Map x)
+        {
+            return new Map()
+            {
+                ID = x.ID,
+                Name = x.Name,
+                AreaName = x.AreaName,
+                DateTimeDescription = x.DateTimeDescription,
+                DefendingTeam = x.DefendingTeam,
+                Description = x.Description,
+                SkirmishImagePath = x.SkirmishImagePath,
+            };
+        }
     }
 }
