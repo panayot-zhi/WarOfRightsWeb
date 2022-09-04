07f78f18a9ef1c71a45747ab7aa66ca47e7ad3f2
Panayot Ivanov <panayot_zhi@abv.bg>
2022-09-04 17:13:56 +0300

    Add SlashCommandsModule.

---
+++ Utility/DiscordSlashCommandsModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Discord.Interactions;
+using System.Threading.Tasks;
+
+namespace WarOfRightsWeb.Utility
+{
+    public class DiscordSlashCommandsModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("next-event", "When is the next event?")]
+        public async Task NextEvent()
+        {
+            await RespondAsync(DateTime.Now.ToString("O"));
+        }
+
+        [SlashCommand("next-event-announcement", "When will be the next event announcement?")]
+        public async Task NextEventAnnouncement()
+        {
+            await RespondAsync(DateTime.Now.AddHours(-1).ToString("O"));
+        }
+    }
+}
