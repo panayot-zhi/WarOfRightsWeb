61127a424ab3d8a3dafe8b2b0c4c40932758d769
Panayot Ivanov <panayot_zhi@abv.bg>
2022-09-04 22:01:12 +0300

    Next event command.

--- Utility/DiscordSlashCommandsModule.cs
+++ Utility/DiscordSlashCommandsModule.cs
@@ -1,21 +1,38 @@
 ﻿using System;
+using System.Linq;
 using Discord.Interactions;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using WarOfRightsWeb.Common;
 
 namespace WarOfRightsWeb.Utility
 {
     public class DiscordSlashCommandsModule : InteractionModuleBase<SocketInteractionContext>
     {
+        private readonly IConfiguration _configuration;
+
+        public DiscordSlashCommandsModule(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [SlashCommand("next-event", "When is the next event?")]
         public async Task NextEvent()
         {
-            await RespondAsync(DateTime.Now.ToString("O"));
-        }
+            var eventTemplates = _configuration.GetEventTemplates();
+            var events = Extensions.GetEventsByDate(eventTemplates, DateTime.Now.Date);
+            if (!events.Any())
+            {
+                await RespondAsync("No events have been found. :(");
+            }
+            else
+            {
+                var nextEvent = events.First();
+                var eventTime = TimeZoneInfo.ConvertTime(nextEvent.Starting, 
+                    Extensions.GetCentralEuropeanTimeZoneInfo());
 
-        [SlashCommand("next-event-announcement", "When will be the next event announcement?")]
-        public async Task NextEventAnnouncement()
-        {
-            await RespondAsync(DateTime.Now.AddHours(-1).ToString("O"));
+                await RespondAsync($"Next event will be at {eventTime.ToString("MMMM d, h tt")} CET.");
+            }
         }
     }
 }
