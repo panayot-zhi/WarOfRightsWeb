1b8b23b77cda0800de3290152fcbffa529da2e03
Panayot Ivanov <panayot_zhi@abv.bg>
2022-09-04 16:52:28 +0300

    Add HostedServiceWrapper helper.

---
+++ Utility/HostedServiceWrapper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace WarOfRightsWeb.Utility
+{
+    public class HostedServiceWrapper<TService> : IHostedService
+    {
+        private readonly IHostedService _hostedService;
+
+        public HostedServiceWrapper(TService hostedService)
+        {
+            _hostedService = (IHostedService) hostedService;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return _hostedService.StartAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return _hostedService.StopAsync(cancellationToken);
+        }
+    }
+}
