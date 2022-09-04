e67f845d6ab914df989784dc1ccc71f93b52a7f6
Panayot Ivanov <panayot_zhi@abv.bg>
2022-09-04 17:13:41 +0300

    Add DiscordConfig.

---
+++ Models/DiscordConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace WarOfRightsWeb.Models
+{
+    public class DiscordConfig
+    {
+        public ulong GuildId { get; set; }
+
+        public string BotToken { get; set; }
+        
+        public ulong AnnouncementChannelId { get; set; }
+    }
+}
